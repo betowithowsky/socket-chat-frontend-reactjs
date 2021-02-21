15e7ff5e245162277aade2cf65e257dd0b09c57f
Roberto Withowsky <betowithowsky@gmail.com>
2021-02-21 00:45:31 -0300

    feat: create a nickname and login

--- src/Chat.js
+++ src/Chat.js
@@ -1,87 +1,121 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-    
-const Chat = ({socket}) => {
-  
-  const [usersOnline, setUsersOnline] = useState([])
-  const [targetUser, setTargetUser] = useState({})
-  const [currentUser, setCurrentUser] = useState({userId: null, username: '', nickname: ''})
-  const [currentMessage, setCurrentMessage] = useState({ 
-    content: '',
-    from: '',
-    time: '',
-    to: currentUser.userId
-  })
-  const [messages, setMessages] = useState([])
+const Chat = ({ socket }) => {
+  //config_user
+  const [userConfig, setUserConfig] = useState({ name: "" });
+  const [hasLoged, sethasLoged] = useState(false);
+
+  const [usersOnline, setUsersOnline] = useState([]);
+  const [targetUser, setTargetUser] = useState({});
+  const [currentUser, setCurrentUser] = useState({
+    userId: null,
+    username: "",
+    nickname: "",
+  });
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  function setupBeforeUnloadListener() {
+    window.addEventListener("beforeunload", (ev) => {
+      ev.preventDefault();
+      return socket.emit("user:logout");
+    });
+  }
+
+  function login(){
+    socket.emit("user:login", userConfig);
+    socket.emit("users:get");
+    sethasLoged(true)
+  }
 
   useEffect(() => {
-    socket.emit('user:login')
-    socket.emit('users:get')
-
-    socket.on("response", ({type, data}) => {
-      console.log('algum on', type, data)
-      if(type === 'user:added'){
-        setUsersOnline((prevState) => ([...prevState, data]))
-      }
-      if(type === 'user:login'){
-        setCurrentUser(data)
-      }
-      if(type === 'message:new'){
-        console.log('new', data)
-        setMessages((prevState) => ([...prevState, data]))
-      }
-      if(type === 'message:send'){
-        console.log('send', data)
-        setMessages((prevState) => ([...prevState, data]))
-      }
-      if(type === 'users:get'){
-        setUsersOnline(data)
-      }
-      if(type === 'user:removed'){
-        const userOnlineFiltred = usersOnline.filter(el => el.userId === data.userId)
-        setUsersOnline(userOnlineFiltred)
-      }
-      
-    });
-
+      socket.on("response", ({ type, data }) => {
+        console.log("algum on", type, data);
+        if (type === "user:added") {
+          setUsersOnline((prevState) => [...prevState, data]);
+        }
+        if (type === "user:login") {
+          setCurrentUser(data);
+        }
+        if (type === "message:new") {
+          console.log("new", data);
+          setMessages((prevState) => [...prevState, data]);
+        }
+        if (type === "message:sendAll") {
+          console.log("sendAll", data);
+          setMessages((prevState) => [...prevState, data]);
+        }
+        if (type === "users:get") {
+          setUsersOnline(data);
+        }
+        if (type === "user:removed") {
+          const userOnlineFiltred = usersOnline.filter(
+            (el) => el.userId === data.userId
+          );
+          setUsersOnline(userOnlineFiltred);
+        }
+      });
+      setupBeforeUnloadListener();
   }, [socket]);
 
   const setUserToSendMessage = (targetUser) => {
-    console.log(targetUser)
-    setTargetUser(targetUser)
-  }
+    console.log(targetUser);
+    setTargetUser(targetUser);
+  };
 
   const onSendMessage = () => {
-    if(currentMessage){
-      socket.emit("message:send", { 
+    if (currentMessage) {
+      socket.emit("message:send", {
         content: currentMessage,
         from: currentUser.userId,
+        nickname: currentUser.nickname,
         time: new Date().getTime(),
-        to: targetUser.userId
-      })
-      setCurrentMessage({ 
-        ...currentMessage,
-        content: '',
-      })
+        to: targetUser.userId,
+      });
+      setCurrentMessage("");
     }
-  }
-  
-    return (
-    <>
-    <div>{usersOnline.map((user, index) => <div key={index} onClick={() =>setUserToSendMessage(user) }>{user.nickname}</div>)}</div>
-    <div>{`username: ${currentUser.username} - nickname: ${currentUser.nickname} - userid: ${currentUser.userId}`}</div>
-        <div>
-          <ul id="messages">
-            {messages.map((el, index) => {
-              return <li key={index}>{`${el.from}: ${el.content}`}</li>
-            })}
-          </ul>
-          <div id="form">
-            <input id="input" value={currentMessage.content} onChange={(e) => setCurrentMessage(e.target.value)} /><button onClick={onSendMessage}>Send</button>
-          </div>
-        </div>
-        </>
-    )
-}
+  };
 
-export default Chat
+  if (!hasLoged) {
+    return (
+      <div id="form">
+        <input
+          id="input"
+          value={userConfig?.name}
+          onChange={(e) => setUserConfig({ name: e.target.value })}
+        />
+        <button onClick={login}>Entrar</button>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div>
+        {usersOnline.map((user, index) => (
+          <div key={index} onClick={() => setUserToSendMessage(user)}>
+            {user.nickname}
+          </div>
+        ))}
+      </div>
+      <div>{`username: ${currentUser.username} - nickname: ${currentUser.nickname} - userid: ${currentUser.userId}`}</div>
+      <div>
+        <ul id="messages">
+          {messages.map((el, index) => {
+            return <li key={index}>{`${el.nickname}: ${el.content}`}</li>;
+          })}
+        </ul>
+        <div id="form">
+          <input
+            id="input"
+            value={currentMessage}
+            onChange={(e) => setCurrentMessage(e.target.value)}
+          />
+          <button onClick={onSendMessage}>Send</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Chat;
